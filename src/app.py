179482d4189927e8b46ddea1d648590b23c4f71e
noman007-dev <itsme.noman007@gmail.com>
2026-06-21 01:45:50 +0600

    update bug

--- src/app.py
+++ src/app.py
@@ -1,28 +1,25 @@
-from google.protobuf import message
 import streamlit as st
 from openai import OpenAI
 from google import genai
 from groq import Groq
-from dotenv import load_dotenv
-from urllib3 import response
 from core.config import config
+
 
 def runllm(provider, model_name, messages, max_tokens=500):
     if provider == "Openai":
         client = OpenAI(api_key=config.openai_api_key)
-    
     elif provider == "Groq":
         client = Groq(api_key=config.groq_api_key)
     else:
         client = genai.Client(api_key=config.gemini_api_key)
-    
+
     if provider == "Google":
-        response = client.models.generate_content(
+        return client.models.generate_content(
             model=model_name,
-            contents=[message["content"] for message in messages],
+            contents=[m["content"] for m in messages if m["role"] != "system"],
         ).text
     elif provider == "Groq":
-        response = client.chat.completions.create(
+        return client.chat.completions.create(
             model=model_name,
             messages=messages,
             max_tokens=max_tokens
@@ -34,6 +31,7 @@ def runllm(provider, model_name, messages, max_tokens=500):
             max_completion_tokens=max_tokens,
             reasoning_effort="minimal",
         ).choices[0].message.content
+
 
 with st.sidebar:
     st.title("LLM API Tester")
@@ -48,21 +46,26 @@ with st.sidebar:
     st.session_state.provider = provider
     st.session_state.model_name = model_name
 
+
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "system", "content": "You are a helpful assistant."}]
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+for msg in st.session_state.messages:
+    if msg["role"] != "system":
+        with st.chat_message(msg["role"]):
+            st.markdown(msg["content"])
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-    
+
     with st.chat_message("assistant"):
-        output = runllm(st.session_state.provider, st.session_state.model_name, st.session_state.messages)
-        response_data = output
-        answer = response_data
+        answer = runllm(
+            st.session_state.provider,
+            st.session_state.model_name,
+            st.session_state.messages
+        )
         st.write(answer)
-    st.session_state.messages.append({"role":"assistant","content":answer})
+
+    st.session_state.messages.append({"role": "assistant", "content": answer})
